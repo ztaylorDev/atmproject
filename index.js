8920507e8a6851a14202db78a841c538ada0814f
Zachary Taylor <zpanther8920@yahoo.com>
2021-07-04 22:24:09 -0400

    created a pin validation function

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { getBal, withDr, depo } = require('./atm')
 const atm = require('./atm')
 const prompt = require('prompt-sync')()
 const {validatePin} = require("./atm")
-
 atm.validatePin()
+
 
 let userInput = prompt("Would you like to see your funds? yes or no. ")
     if(userInput == "yes"){
@@ -23,7 +23,14 @@ let userInput2 = prompt("Would you like to deposit, withdraw, or exit. ")
             withDr(parseInt(prompt("how much would you like to withdraw. ")))   
         break;    
 
+       
+
+
     }
+
+
+
+
 
     // if(userInput2 == "deposit"){
     //     depo(parseInt(prompt("how much would you like to deposit. ")))
